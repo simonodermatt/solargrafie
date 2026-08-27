8bc2d970d27fe1b4e74fe65cb156b4cb68b3ff85
Odermatt Simon <sp.odermatt@gmail.com>
2026-08-27 13:36:08 +0200

    MAP UPDATE

--- translations.js
+++ translations.js
@@ -6,7 +6,12 @@ const translations = {
     note: "Notiz",
     extId: "Ext. ID",
     viewLarge: "Großansicht",
-    openGPhotos: "In Google Fotos öffnen →"
+    openGPhotos: "In Google Fotos öffnen →",
+    regions: {
+      ch: "Schweiz",
+      eu: "Europa",
+      world: "Welt"
+    }
   },
   en: {
     header: "SOLARGRAPHY PROJECT (Eva & Simon)",
@@ -15,7 +20,12 @@ const translations = {
     note: "Note",
     extId: "Ext. ID",
     viewLarge: "Full View",
-    openGPhotos: "Open in Google Photos →"
+    openGPhotos: "Open in Google Photos →",
+    regions: {
+      ch: "Switzerland",
+      eu: "Europe",
+      world: "World"
+    }
   },
   fr: {
     header: "PROJET SOLARGRAPHIE (Eva & Simon)",
@@ -24,7 +34,12 @@ const translations = {
     note: "Note",
     extId: "ID ext.",
     viewLarge: "Agrandir",
-    openGPhotos: "Ouvrir dans Google Photos →"
+    openGPhotos: "Ouvrir dans Google Photos →",
+    regions: {
+      ch: "Suisse",
+      eu: "Europe",
+      world: "Monde"
+    }
   },
   it: {
     header: "PROGETTO SOLARGRAFIA (Eva & Simon)",
@@ -33,7 +48,12 @@ const translations = {
     note: "Nota",
     extId: "ID est.",
     viewLarge: "Ingrandisci",
-    openGPhotos: "Apri in Google Foto →"
+    openGPhotos: "Apri in Google Foto →",
+    regions: {
+      ch: "Svizzera",
+      eu: "Europa",
+      world: "Mondo"
+    }
   },
   uk: {
     header: "ПРОЄКТ СОЛЯРОГРАФІЯ (Єва та Симон)",
@@ -42,7 +62,12 @@ const translations = {
     note: "Примітка",
     extId: "Зовн. ID",
     viewLarge: "Переглянути",
-    openGPhotos: "Відкрити в Google Фото →"
+    openGPhotos: "Відкрити в Google Фото →",
+    regions: {
+      ch: "Швейцарія",
+      eu: "Європа",
+      world: "Світ"
+    }
   },
   ru: {
     header: "ПРОЕКТ СОЛЯРОГРАФИЯ (Ева и Симон)",
@@ -51,6 +76,11 @@ const translations = {
     note: "Заметка",
     extId: "Внеш. ID",
     viewLarge: "Просмотреть",
-    openGPhotos: "Открыть в Google Фото →"
+    openGPhotos: "Открыть в Google Фото →",
+    regions: {
+      ch: "Швейцария",
+      eu: "Европа",
+      world: "Весь мир"
+    }
   }
 };
